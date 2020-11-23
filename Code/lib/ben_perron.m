7e3e49c48e2fc5e4fc4619f33f98ce79a5bb4ef1
Raj9898 <rajesh.rao.rs9@gmail.com>
2020-11-23 18:07:39 -0500

    Revert "Modified code for structural changes"
    This reverts commit 1228e8ca07e244bdb4aa5982e6cdb8b92cb73d48.

--- Code/lib/ben_perron.m
+++ Code/lib/ben_perron.m
@@ -1,33 +1,5 @@
-%
-% Author, Rajesh Rao
-% 
-% Test for structural breaks in linear models as detailed in the Bai and 
-% Perron (1998, 2003 a,b) paper
-% ------------------------------------------------------------------------
-%
-%
-%
-%
+% Test for structural breaks in linear models as detailed in the Bai and Perron (1998, 2003a,b) paper
 
-function ben_perron(y, X)
-    
-     % check the variable type entered 
-    assert(isa(y, 'double'), ...
-        'TypeError: independent variable must be of type double');
-    assert(isa(X, 'double'), ...
-        'TypeError: dependent variable must be of type double');
-    
-    % check the dimensionality and case of function parameters
-    assert(size(y, 1) == size(X, 1), ...
-        'both variables must have the same number of observations'); 
-    
-    % compute stationary variables
-    [T, ~] = size(X);
-    maxBreaks = T * ( T + 1 ) / 2;
-    
-    % compute the OLS residuals from regression
-    [~,~,residuals] = regress(y, X); 
-    squaredResiduals = residuals .^ 2;
-     
-    
+function ben_perron()
+
 end
